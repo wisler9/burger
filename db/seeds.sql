e6b2d201ba2883975122dfa53485ede853ae5e10
Logan <wislermichel@gmail.com>
2019-12-02 07:10:03 -0500

    working final version

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name,devoured) VALUES ("SpicyDeluxe burger", false);
+INSERT INTO burgers (burger_name,devoured) VALUES ("DoubleCheese burger", false);
+INSERT INTO burgers (burger_name,devoured) VALUES ("BaconDeluxe burger", false);
+INSERT INTO burgers (burger_name,devoured) VALUES ("Whopper burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon avocado Cheeseburger', false);
--- server.js
+++ server.js
@@ -1,21 +1,29 @@
-var express = require("express");
+// Pull in required dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var port = process.env.PORT || 3000;
+
 var app = express();
-var PORT = process.env.PORT || ;
 
+// Serve static content for the app from the 'public' directory
+app.use(express.static(process.cwd() + '/public'));
 
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
 
-var exphbs = require("express-handlebars");
+// Set Handlebars as the view engine
+var exphbs = require('express-handlebars');
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
+// Import routes and give the server access to them
+var routes = require('./controllers/burgers_controller.js');
 
-app.use(routes);
+app.use('/', routes);
 
-app.listen(PORT,function(){
-    console.log("server is listening to PORT: "+ PORT);
-})
+app.listen(port);
